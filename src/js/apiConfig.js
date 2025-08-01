c0f60ac4311bc0725048c1f8c43e2af603a23d00
LuanAssis01 <assisluan863@gmail.com>
2025-07-31 22:41:11 -0300

    fix: ajusted

--- src/js/apiConfig.js
+++ src/js/apiConfig.js
@@ -3,12 +3,7 @@
 // Força o uso das configurações de produção
 export const API_BASE_URL = 'https://anuli-backend.onrender.com'; // URL para o site no ar (produção)
 
-// Em produção, o caminho base é vazio.
-export const BASE_PATH = '';
-
 // Constantes para as rotas do frontend, usando o caminho base de produção
-export const LOGIN_PAGE_URL = `${BASE_PATH}/src/html/auth/login.html`;
+export const LOGIN_PAGE_URL = 'src/html/auth/login.html';
 
-// Informa que a API está configurada para produção
-// console.log('API rodando em modo: Produção');
-// console.log(`URL da API: ${API_BASE_URL}`);
+
