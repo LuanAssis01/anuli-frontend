dad0e146751b06488672317248caef0f3a9f2be4
LuanAssis01 <assisluan863@gmail.com>
2025-08-16 14:48:23 -0300

    fix: apiConfig.js

--- src/js/apiConfig.js
+++ src/js/apiConfig.js
@@ -1,15 +1,12 @@
 // src/js/apiConfig.js
 
-// Verifica se o site está a ser executado no ambiente local (desenvolvimento)
 const isDevelopment = window.location.hostname === '127.0.0.1' || window.location.hostname === 'localhost';
 
-// Define a URL base da API com base no ambiente
 export const API_BASE_URL = isDevelopment
-  ? 'http://127.0.0.1:3000' // URL para a sua máquina
-  : 'https://anuli-backend.onrender.com'; // URL para o site em produção
+  ? 'http://127.0.0.1:3000'
+  : 'https://api.anuliacessorios.com.br';
 
-// --- Constantes para as rotas do frontend ---
-// Usamos caminhos absolutos para garantir que funcionem a partir de qualquer página.
+
 export const LOGIN_PAGE_URL = '/src/html/auth/login.html';
-export const ADMIN_DASHBOARD_URL = '/src/html/admin/dashboard.html'; // <-- NOVO
-export const USER_ACCOUNT_URL = '/src/html/conta/minha_conta.html';    // <-- NOVO
+export const ADMIN_DASHBOARD_URL = '/src/html/admin/dashboard.html'; 
+export const USER_ACCOUNT_URL = '/src/html/conta/minha_conta.html';   
